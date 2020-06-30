f3c615683b6d9761327d299de32ca8eff0761204
calebelson <caleb.elson@gmail.com>
2020-06-29 22:14:41 -0500

    Initial Commit

---
+++ ninety days/ninety_daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  ninety_daysApp.swift
+//  ninety days
+//
+//  Created by Caleb Elson on 6/29/20.
+//
+
+import SwiftUI
+
+@main
+struct ninety_daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
